a8671c86dcdd2762a8d0c843e0e75f6cbebf26f1
Danilov Nikolai <mailabc.ru@yandex.ru>
2019-10-15 22:58:57 +0300

    added a simple timer

--- js/main.js
+++ js/main.js
@@ -32,4 +32,49 @@ window.addEventListener("DOMContentLoaded", function() {
             }
         }
     });
+
+    //Timer
+
+    let deadLine = "2019-10-21";
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60), //Кол-во секунд из миллисекунд
+            minuts = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+            // hours = Math.floor((t/1000/60/60) % 24)
+            // days = Math.floor((t/(1000*60*60*24)))
+            return {
+                "total" : t,
+                "hours" : hours,
+                "minutes" : minuts,
+                "seconds" : seconds
+            };
+    }
+
+    //Превращаем статическую вёрстку в динамическую
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector(".hours"),
+            minutes = timer.querySelector(".minutes"),
+            seconds = timer.querySelector(".seconds"),
+            timerInterval = setInterval(updateClock, 1000);
+
+        function updateClock () {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if(t.total <= 0) {
+                clearInterval(timerInterval);
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+            }
+        }
+    }
+
+    setClock("timer", deadLine);
 });
